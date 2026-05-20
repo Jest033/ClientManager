35a9081182b0a7f717b2feccd5a1e5a85e2797ad
David <david10sanya11@xmail.ru>
2026-05-20 14:08:53 +0300

    // Service with full CRUD operations, search, filter and sort functionality

--- ClientManager/Services/ClientService.cs
+++ ClientManager/Services/ClientService.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 using ClientManager.Models;
-
+// Service with full CRUD operations, search, filter and sort functionality
 namespace ClientManager.Services
 {
     public class ClientService
